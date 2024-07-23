e5cb2bf97a743a2bc96436742ce88e74d89bcd71
cjph96 <cristian.perez@deliverea.com>
2024-07-23 17:13:32 +0100

    feat: create project with Spring Initializr

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Inditex Technical Test
+Technical test for Inditex: Using Java with Spring Framework
---
+++ src/test/java/com/inditex/technicaltest/TechnicalTestApplicationTestCase.java
@@ -0,0 +1,7 @@
+package com.inditex.technicaltest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public abstract class TechnicalTestApplicationTestCase {
+}
